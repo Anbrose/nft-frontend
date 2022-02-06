3553035d4b71d67e23f5a5e64d860741cf932fc9
z5142177@student.unsw.edu.au <z5142177@student.unsw.edu.au>
2022-02-06 18:30:37 +1100

    syn

--- src/components/nftdashboard/nft-list-historychart.js
+++ src/components/nftdashboard/nft-list-historychart.js
@@ -1,39 +1,80 @@
+import React, { useState, useEffect} from 'react'
 import { Line } from 'react-chartjs-2'
 import { Card, CardContent, Grid } from '@mui/material';
 
-export const NftListHistorychart = () => (
-  <Card>
-    <CardContent>
-      <Grid item>
-        <Line
-          data={{
-            labels: [1,2,3,4,5,6],
-            datasets: [
-              {
-                label: "Test data",
-                data: [2,4,5,5,6,7,8],
-                tension: 0.4
-              }
-            ]
-          }}
-          height={400}
-          width={600}
-          options={{
-            scales:{
-              xAxes: [
-                {
-                  ticks: {
-                    autoSkip: true,
-                    maxTicksLimit: 4
-                  }
-                }],
-              yAxes: [
-                {
-                  position: 'right',
-                }],
-            }
-          }}/>
-      </Grid>
-    </CardContent>
-  </Card>
-)
+export const NftListHistorychart = () => {
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function getRandomInts(len){
+    let re = []
+    for (let i = 0; i < len; i++){
+      re.push(getRandomInt(100))
+    }
+    return re
+  }
+
+  function getTestLable(len){
+    let re = []
+    for (let i = 0; i < len; i++){
+      re.push("Date" + i)
+    }
+    return re
+  }
+
+  const [historyLabels, sethistoryLabels] = useState(getTestLable(1000))
+  const [historyData, sethistoryData] = useState(getRandomInts(1000))
+
+
+  const NFTLineChartOption = {
+    options:{
+      elements:{
+        point:{
+          radius: 0
+        }
+      }
+    },
+    scales:{
+      x: 
+        {
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 4
+          }
+        },
+      y: {
+        display: true,
+        position: 'right'
+      }
+    }
+  }
+
+  const NFTLineChartData = {
+    labels: historyLabels,
+    datasets: [
+      {
+        label: "Test data",
+        data: historyData,
+        tension: 0.4,
+        yAxisID: "y"
+      }
+    ]
+  }
+
+  return (
+    <Card>
+      <CardContent>
+        <Grid item>
+          <Line
+            data={NFTLineChartData}
+            height={600}
+            width={800}
+            options={NFTLineChartOption}/>
+        </Grid>
+      </CardContent>
+    </Card>
+  )
+}
+
